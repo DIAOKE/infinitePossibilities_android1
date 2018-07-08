4227be036a5010baaddf580c1501dd984e004192
DIAOKE <1358048886@qq.com>
2018-07-08 17:39:28 +0800

    注释//

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,9 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
 
-/*gulp.task('default', ['praise'], () =>
-    gulp.watch(['src/**/*.es6', '!src/public/*/*.es6'],['praise'])
-);*/
+// gulp.task('default', ['praise'], () =>
+//     gulp.watch(['src/**/*.es6', '!src/public/*/*.es6'],['praise'])
+// )
 
 gulp.task('praise', () => {
     return gulp.src(['src/**/*.es6', '!src/public/*/*.es6'])
